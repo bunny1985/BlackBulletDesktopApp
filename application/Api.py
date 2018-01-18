265329d0d88b2b4385123515682d244ce71a7ee4
Michał Banaś <mb.michal.banas@gmail.com>
2018-01-18 22:41:16 +0100

    refactoring

---
+++ application/Api.py
@@ -0,0 +1,128 @@
+import json
+from collections import namedtuple
+import httplib2
+import websocket
+from Utils import SettinsManager
+import threading
+import time
+import gi
+
+gi.require_version('Gtk', '3.0')
+
+from gi.repository import  GObject
+
+import logging
+logging.basicConfig( level=logging.DEBUG)
+log = logger = logging.getLogger(__name__)
+
+
+class ConnectivityManager(GObject.GObject):
+    checker_thread = None
+    checker_thread_cancelation_token = None
+    ws = None
+
+    __gsignals__ = {
+        'authenticated': (GObject.SIGNAL_RUN_LAST, None,(str,)),
+        'authenticatation_failed': (GObject.SIGNAL_RUN_LAST, None,(str,)),
+        'ws_opened': (GObject.SIGNAL_RUN_LAST, None ,(str,)),
+        'ws_error': (GObject.SIGNAL_RUN_LAST, None ,(str,)),
+        'ws_closed': (GObject.SIGNAL_RUN_LAST, None  ,(str,)),
+        'ws_message_recived': (GObject.SIGNAL_RUN_LAST, None  ,(str,))
+    }
+    
+
+    def __init__(self , app):
+        GObject.GObject.__init__(self)
+        self.app = app
+        self.http = httplib2.Http(".cache")
+        self.config = SettinsManager().get_config_as_object()
+        
+
+        
+    def start_websocket_health_checker(self):
+        def check_healthThread():
+            while(True):
+                if self.checker_thread_cancelation_token != None : 
+                    break
+                if  self.ws.isOk  : 
+                    log.debug("HealthCheck: Websocket is ok")
+                else:
+                    log.debug("HealthCheck: Websocket is broken")
+                    self.ws.close()
+                    ConnectivityManager.websocketWorker = threading.Thread(name='worker', target=self.ws.run_forever)
+                    ConnectivityManager.websocketWorker.daemon = True
+                    ConnectivityManager.websocketWorker.start()
+                
+                time.sleep(5)
+
+        print("starting helath Checker")
+        self.checker_thread = threading.Thread(name = "wsHealthChecker" , target = check_healthThread)
+        self.checker_thread.start()
+    
+
+    def authorize(self):
+        try:
+            (resp_headers, content) = self.http.request("http://" +  self.config.address + "/api/Account/Login" , "POST", body = "{'email': '" + self.config.user_name+ "', 'PASSWORD': '" + self.config.password+  "'}", headers = {'Content-type': 'application/json'})
+            self.authCookie =  resp_headers["set-cookie"]
+            self.app.state.authorized = True
+            
+            self.emit("authenticated" , self.authCookie)
+        except Exception as e:
+            print(e)
+            self.emit("authenticatation_failed" , "Something went wrong")
+            
+    
+
+    def connect_web_socket(self):
+        websocket.enableTrace(True)
+        self.ws = websocket.WebSocketApp(url = "ws://" + self.config.address + "/notificationsSocket" , on_open = self.on_connect_callback()  ,on_close=self.on_disconnect_callback(),  cookie = self.authCookie )
+        self.ws.on_message = self.on_message_callback()
+        self.ws.isOk = False
+        self.start_websocket_health_checker()
+    
+
+        
+    def on_connect_callback(self):
+        def fun(ws):
+            log.debug("connected")
+            self.ws.isOk = True
+            self.emit("ws_opened" , "Ok")
+            #GObject.idle_add(lambda :self.emit("ws_opened" , "Closed"))
+        return fun
+
+    
+        
+    def on_disconnect_callback(self):
+        
+        def fun(ws):
+            log.debug("disconnected")
+            GObject.idle_add(lambda :self.emit("ws_closed" , "Closed"))
+            self.ws.isOk = False
+        return fun
+    
+
+            
+
+    
+    def ringtone_request(self):
+        self.ws.send('{"type": "ringtone"}')
+    def sms_request(self, to , text):
+        self.ws.send("{'type': 'sms' , 'to' : '" + to + "' , 'text': '" + text+ "' }")
+    def share_request(self, text):
+        self.ws.send("{'type': 'notification' , 'text': '" + text+ "' }")
+    
+
+
+    def on_message_callback(self):
+        def on_message(ws, message):
+            try:
+                print("Server Said: ")
+                log.debug("WS message:" + message)
+                #msg = json.loads(message , object_hook=lambda d: namedtuple('X', d.keys())(*d.values()) )
+                #self.app.notify(msg.title , msg.body , "information" )
+                self.emit("ws_message_recived" , message)
+            except:
+                pass
+
+        return on_message
+            
---
+++ application/Application.py
@@ -0,0 +1,124 @@
+import gi
+
+gi.require_version('Gtk', '3.0')
+
+from gi.repository import Gtk, Gdk
+
+from gi.repository import Gio
+import sys
+import Ui
+import Utils
+import Api
+import logging
+import time 
+logging.basicConfig( level=logging.DEBUG)
+log = logger = logging.getLogger(__name__)
+
+class AppState():
+    authorized = False
+    connected = False
+    
+
+class BlackBulletApplication(Gtk.Application):
+    APPNAME = "boilerplate"
+    
+
+    
+    def __init__(self):
+        self.state = AppState()
+        
+        self.notification_factory = Ui.NotificatonFactory()
+        Gtk.Application.__init__(self)
+        Gdk.threads_init()
+
+    def do_startup(self):
+        Gtk.Application.do_startup(self)
+
+        #self.settings = Gio.Settings.new(self.SETTINGS_KEY)
+        self.builder = Gtk.Builder()
+        self.builder.add_from_file("../resources/GladeFiles/SmsWindow.glade")
+        if self.is_config_valid() == False: 
+            self.show_settings_window()
+        self.api = Api.ConnectivityManager(self)
+        self.api.connect("authenticated" , self.on_authorized)
+        self.api.connect("authenticatation_failed" , self.on_authorization_failed)
+        self.api.connect("ws_opened" , self.on_ws_opened)
+        self.api.connect("ws_closed" , self.on_ws_closed)
+        self.api.connect("message_recived" , self.on_message_recived)
+        
+        self.api.authorize()
+        
+    def reset_connectivity(self):
+        log.info("Closing Connections")
+        self.api.checker_thread_cancelation_token = True
+        if self.api.ws!=None:
+            self.api.ws.close()
+        # Make sure that everything is closed
+        time.sleep(5)
+        log.info("Opening Connections")
+        self.api.checker_thread_cancelation_token = None
+        self.api.authorize()
+        
+
+    def on_ws_opened(self , event ,msg ):
+        log.info("WS OPEN! WE ARE ONLINE NOW")
+    def on_ws_closed(self , event ,reason ):
+        log.info("CONNECTION DOWN")
+
+    def on_message_recived(self, event , rawmmessage)
+        
+
+    def on_authorized(self , event ,cookie ):
+        log.info("Authenticated")
+        self.api.connect_web_socket()
+
+    def on_authorization_failed(self, event ,string):
+        log.warn("AUTH FAILED")
+        self.notification_factory.CreateWarning("Authorization Failed"  , "").show()
+        self.show_settings_window()
+
+
+    def is_config_valid(self):
+        config = Utils.SettinsManager().get_config_as_object()
+        if config.user_name == "" or config.password == "" or config.address == "":
+            return False
+        return True
+
+
+    def show_sms_window(self):
+        Ui.SmsWindow(self)
+    def show_send_window(self):
+        Ui.SendWindow(self)
+    def show_settings_window(self):
+        Ui.SettingsWindow(self)
+    def play_ringtone(self):
+        self.notify("Playing ringtone request" , "Sending" , "info")
+        self.connectivity.ringtone_request()
+    def sms(self, to , text):
+        self.notify("Sending Sms via phone" , "" , "info")
+        self.connectivity.sms_request(to, text);
+    def share(self,  text):
+        self.notify("Sending info to phone" ,"", "info")
+        self.connectivity.share_request(text);
+
+    def do_activate(self):
+        self.icon = Ui.TrayIcon(self)
+        self.notification_factory.CreateInfo("Blackbullet Started" , "").show()
+        Gtk.main()
+
+        
+        
+        
+        
+    def quit(self):
+        self.api.checker_thread_cancelation_token = True
+        self.notification_factory.CreateInfo("Bye Bye" , "").show()
+        Gtk.main_quit()
+        # self.connectivity.ws.close()
+
+        sys.exit(0)
+
+
+        
+
+      
